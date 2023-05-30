6a4ed447c806c6e3059d59b37ae7737c15988bd7
donfiguerres <lyndonfiguerres@gmail.com>
2023-05-30 23:59:09 +0800

    Initial commit

---
+++ src/test/java/com/europeanexchangerates/exchangeapi/ExchangeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.europeanexchangerates.exchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
