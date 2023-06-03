60006a2ed885da764a942b19a5bbed249e8c8470
donfiguerres <lyndonfiguerres@gmail.com>
2023-06-04 03:10:19 +0800

    Remove unused import

--- src/test/java/com/europeanexchangerates/exchangeapi/controller/ExchangeRateControllerTest.java
+++ src/test/java/com/europeanexchangerates/exchangeapi/controller/ExchangeRateControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.europeanexchangerates.exchangeapi.dto.CurrencyAverageRate;
